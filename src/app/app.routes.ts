f0354016999c55d108af12e4b7a1489ce55f3598
christophthonhauser <c.thonhauser97@gmail.com>
2025-04-03 00:09:49 +0200

    add eslint and prettier

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,33 +1,48 @@
-import {Routes} from '@angular/router';
+import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
     path: 'rechner',
-    loadComponent: () => import('./features/rechner/rechner-page/rechner-page.component').then(m => m.RechnerPageComponent)
+    loadComponent: () =>
+      import('./features/rechner/rechner-page/rechner-page.component').then(
+        (m) => m.RechnerPageComponent,
+      ),
   },
   {
     path: 'check',
-    loadComponent: () => import('./features/check/check-page/check-page.component').then(m => m.CheckPageComponent)
+    loadComponent: () =>
+      import('./features/check/check-page/check-page.component').then(
+        (m) => m.CheckPageComponent,
+      ),
   },
   {
     path: 'notar',
-    loadComponent: () => import('./features/notar/notar-page/notar-page.component').then(m => m.NotarPageComponent)
+    loadComponent: () =>
+      import('./features/notar/notar-page/notar-page.component').then(
+        (m) => m.NotarPageComponent,
+      ),
   },
   {
     path: 'dokumente',
-    loadComponent: () => import('./features/dokumente/dokumente-page/dokumente-page.component').then(m => m.DokumentePageComponent)
+    loadComponent: () =>
+      import(
+        './features/dokumente/dokumente-page/dokumente-page.component'
+      ).then((m) => m.DokumentePageComponent),
   },
   {
     path: 'demo',
-    loadComponent: () => import('./features/demo/demo-page/demo-page.component').then(m => m.DemoPageComponent)
+    loadComponent: () =>
+      import('./features/demo/demo-page/demo-page.component').then(
+        (m) => m.DemoPageComponent,
+      ),
   },
   {
     path: '',
     redirectTo: 'rechner',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
   {
     path: '**',
-    redirectTo: 'rechner'
-  }
+    redirectTo: 'rechner',
+  },
 ];
