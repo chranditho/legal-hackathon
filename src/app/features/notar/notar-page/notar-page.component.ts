3a32f901c48e4676f94f5ccf15cc439e9f6ebc55
Raphi <raphael.arzberger@stud.fh-campuswien.ac.at>
2025-04-05 15:04:36 +0200

    map working

--- src/app/features/notar/notar-page/notar-page.component.ts
+++ src/app/features/notar/notar-page/notar-page.component.ts
@@ -48,7 +48,7 @@ export class NotarPageComponent implements AfterViewInit {
 
   requestUserLocation(): void {
     navigator.geolocation.getCurrentPosition(
-      (position) => {
+      position => {
         this.center = {
           lat: position.coords.latitude,
           lng: position.coords.longitude,
@@ -103,12 +103,14 @@ export class NotarPageComponent implements AfterViewInit {
 
       this.places = results
         .filter(
-          (p): p is google.maps.places.PlaceResult & {
+          (
+            p
+          ): p is google.maps.places.PlaceResult & {
             geometry: { location: google.maps.LatLng };
             name: string;
           } => !!p.geometry?.location && !!p.name
         )
-        .map((p) => ({
+        .map(p => ({
           name: p.name,
           location: p.geometry.location,
           address: p.formatted_address ?? p.vicinity ?? '',
