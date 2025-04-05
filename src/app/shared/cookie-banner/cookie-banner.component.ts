99ee74346cea75a3d4d62b9b852c33aada7b4517
Raphi <raphael.arzberger@stud.fh-campuswien.ac.at>
2025-04-05 19:08:54 +0200

    added cookie banner

--- src/app/shared/cookie-banner/cookie-banner.component.ts
+++ src/app/shared/cookie-banner/cookie-banner.component.ts
@@ -1,3 +1,4 @@
+// path: src/app/shared/cookie-banner.component.ts
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -6,21 +7,23 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   template: `
-    <div *ngIf="showBanner" class="fixed bottom-0 w-full bg-gray-800 text-white px-4 py-3 flex justify-between items-center z-50">
-      <span>
-        We use cookies to improve your experience. By using this site, you accept our use of cookies.
-      </span>
+    <div
+      *ngIf="showBanner"
+      class="fixed bottom-0 w-full bg-gray-800 text-white px-4 py-3 flex justify-between items-center z-50">
+      <span
+        >We use cookies to improve your experience. By using this site, you
+        accept our use of cookies.</span
+      >
       <button
         class="ml-4 bg-green-600 hover:bg-green-500 text-white px-4 py-2 rounded"
-        (click)="acceptCookies()"
-      >
+        (click)="acceptCookies()">
         Accept
       </button>
     </div>
   `,
 })
 export class CookieBannerComponent implements OnInit {
-  public showBanner: boolean = false;
+  public showBanner = false;
   private readonly storageKey = 'cookie-consent';
 
   ngOnInit(): void {
