60060d5525bf5d91a42a404e4870e5375be037ac
christian ninaus <christian.ninaus@stud.fh-campuswien.ac.at>
2025-04-05 11:02:37 +0200

    Formatting

--- src/app/features/rechner/rechner-page/rechner-page.component.ts
+++ src/app/features/rechner/rechner-page/rechner-page.component.ts
@@ -1,9 +1,14 @@
 import { Component, inject } from '@angular/core';
-import {FormBuilder, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatStepperModule} from '@angular/material/stepper';
-import {MatButtonModule} from '@angular/material/button';
+import {
+  FormBuilder,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-rechner-page',
