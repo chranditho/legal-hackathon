efd167405b362b9f464c76bb6c43a57e9fdec3e1
christian ninaus <christian.ninaus@stud.fh-campuswien.ac.at>
2025-04-06 10:15:31 +0200

    Impressum_Datenschutzerklaerung2

--- src/app/features/impressum/impressum-page/impressum-page.component.ts
+++ src/app/features/impressum/impressum-page/impressum-page.component.ts
@@ -8,8 +8,6 @@ import { Component } from '@angular/core';
     :host {
       display: block;
     }
-  `
+  `,
 })
-export class ImpressumPageComponent {
-
-}
+export class ImpressumPageComponent {}
