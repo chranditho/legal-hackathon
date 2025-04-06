6910ba60b833e564db966f2bb315c36cf89085f8
christophthonhauser <c.thonhauser97@gmail.com>
2025-04-06 15:29:50 +0200

    add product cards

--- src/app/features/home/ui/product-card/product-card.component.ts
+++ src/app/features/home/ui/product-card/product-card.component.ts
@@ -1,16 +1,19 @@
 import { Component, input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { RouterLink } from '@angular/router';
+import { NgOptimizedImage } from '@angular/common';
 
 export interface ProductCard {
   title: string;
   text: string;
   imageUrl: string;
+  link: string;
 }
 
 @Component({
   selector: 'app-product-card',
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, RouterLink, NgOptimizedImage],
   templateUrl: './product-card.component.html',
   styles: `
     :host {
